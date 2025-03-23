1e61f7a336b4299cdee22415705d19c15fd896fc
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-23 15:35:58 +0530

    roboshop catalogue

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
+source /data/secrets.txt
+cat /data/secrets.txt
+# to debug
+echo
+echo
+echo
+env
+# end of debug
 if [ -z "$REDIS_HOST" ]; then
   echo Environment REDIS_HOST is missing
   exit 1
